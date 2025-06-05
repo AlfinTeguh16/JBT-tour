feae1571329ad7e0e134782676ec58ab9ad22131
alfin <alfinteguh94@gmail.com>
2025-06-05 21:31:02 +0800

    fix: x-button

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -4,9 +4,7 @@
 ])
 
 @php
-    $isSuccess = session('success'); 
     $base = 'inline-flex items-center justify-center rounded-lg px-4 py-2 text-sm font-medium transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2';
-
     $variants = [
         'primary'   => 'bg-blue-600 text-white hover:bg-blue-700 focus:ring-blue-300',
         'secondary' => 'bg-green-600 text-white hover:bg-green-700 focus:ring-green-300',
@@ -16,9 +14,7 @@
         'disabled'  => 'bg-gray-400 text-gray-700 cursor-not-allowed opacity-50',
         'back'      => 'bg-gray-200 text-gray-800 hover:bg-gray-300 focus:ring-gray-200',
     ];
-
-    $finalVariant = $isSuccess ? 'disabled' : $variant;
-    $classes = $base.' '.$variants[$finalVariant];
+    $classes = $base.' '.$variants[$variant];
 @endphp
 
 @if($variant === 'back' && $href)
@@ -26,10 +22,7 @@
         {{ $slot }}
     </a>
 @else
-    <button
-        {{ $attributes->merge(['class' => $classes]) }}
-        @if($isSuccess) disabled @endif
-    >
-        {{ $isSuccess ? 'Success' : $slot }}
+    <button {{ $attributes->merge(['class' => $classes, 'disabled' => $variant === 'disabled']) }}>
+        {{ $slot }}
     </button>
 @endif
